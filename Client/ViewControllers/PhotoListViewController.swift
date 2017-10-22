5b952caf196d9c4f309a165e555d8e573ce6ac47
hiriorima <g2117001@fun.ac.jp>
2017-10-22 11:30:11 +0900

    Add photo list view mock

---
+++ Client/ViewControllers/PhotoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoListViewController.swift
+//  Client
+//
+//  Created by 会津慎弥 on 2017/10/22.
+//  Copyright © 2017年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
