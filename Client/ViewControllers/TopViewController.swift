32de6e854ea45021dfa53b5a196132936837c0a5
hiriorima <g2117001@fun.ac.jp>
2017-10-22 11:02:55 +0900

    Add Top View mock

---
+++ Client/ViewControllers/TopViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TopViewController.swift
+//  Client
+//
+//  Created by 会津慎弥 on 2017/10/22.
+//  Copyright © 2017年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
