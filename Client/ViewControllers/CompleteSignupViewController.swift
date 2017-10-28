4fb45c17b062474fd4c3cea0038c27ec49a635cc
hiriorima <g2117001@fun.ac.jp>
2017-10-28 22:44:45 +0900

    Add complete sign up view

---
+++ Client/ViewControllers/CompleteSignupViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CompleteSignupViewController.swift
+//  Client
+//
+//  Created by 会津慎弥 on 2017/10/28.
+//  Copyright © 2017年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class CompleteSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
